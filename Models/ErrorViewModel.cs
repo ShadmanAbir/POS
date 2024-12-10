719170e47106942d7562922ba1e7b2a13026fc77
Shadman Sakib <shadmanabir@outlook.com>
2024-12-10 10:46:52 +0600

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UtopiaCatering.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
